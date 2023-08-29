de70be9a04f020e22b02b60cbd7dabd1be4d25ae
mayconUaau <maycon@uaau.digital>
2023-08-29 09:30:45 -0300

    icone cadastro, required video

--- web/dev/vaiseraqui/ci4/app/Views/templates/admin-header.php
+++ web/dev/vaiseraqui/ci4/app/Views/templates/admin-header.php
@@ -190,7 +190,7 @@ if ($acesso['usuarioFK'] > 3) {
                     <? if (in_array(5, $aprovados) || $adminMaster) { ?>
                         <ul class="menu js__accordion">
                             <li class='<?= ($_SESSION["menuAdmin"] == 5) ? 'current active' : '' ?> '>
-                                <a class="waves-effect parent-item js__control" href="#"><i class="menu-icon bi bi-person"></i><span>Cadastros</span><span class="menu-arrow fa fa-angle-down"></span></a>
+                                <a class="waves-effect parent-item js__control" href="#"><i class="menu-icon bi bi-person-circle"></i><span>Cadastros</span><span class="menu-arrow fa fa-angle-down"></span></a>
                                 <ul class="sub-menu js__content">
                                     <li class='<?= ($segments[1] == 'cidade') ? "linha-selected" : "" ?>'><a href="<?= PATHSITE ?>admin/cidade/"><i class="bi-caret-right-fill"></i> Cidades</a></li>
                                     <li class='<?= ($segments[1] == 'anunciante') ? "linha-selected" : "" ?>'><a href="<?= PATHSITE ?>admin/anunciante/"><i class="bi-caret-right-fill"></i> Anunciantes</a></li>
